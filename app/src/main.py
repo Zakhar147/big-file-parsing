939e7e5a6526e469bee7d3e9ea661e93e1299ff0
Zahar <zaharkhachaturyan@gmail.com>
2024-12-11 11:14:35 +0100

    adding txt files to git and added time checkint with loading bar

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,6 @@
 import sys
+import time
+from tqdm import tqdm
 
 from modules.utils.make import make_usage
 from modules.file_parsing.start_parsing import start_parsing
@@ -9,7 +11,19 @@ if __name__ == "__main__":
     else:
         file_path = sys.argv[1]
         try:
-            parsing_result = start_parsing(file_path)
+            with tqdm(total=100, desc="Prarsing file", unit="%", colour="green") as pbar:
+                start_time = time.time() 
+                
+                parsing_result = start_parsing(file_path)
+                
+                end_time = time.time() 
+                elapsed_time = end_time - start_time  
+                
+                for _ in range(10):
+                    time.sleep(0.1)
+                    pbar.update(10)
+                
             print(parsing_result)
+            print(f"\nTime elapsed: {elapsed_time:.2f} seconds")
         except Exception as e:
             print(e)
