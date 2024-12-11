7cfa596b24f4de6aae5ff34e8612f5a1edc0b8a4
Zahar <zaharkhachaturyan@gmail.com>
2024-12-11 10:56:40 +0100

    added outher counters

--- app/src/modules/file_parsing/ChunkProcessor.py
+++ app/src/modules/file_parsing/ChunkProcessor.py
@@ -3,6 +3,11 @@ class ChunkProcessor:
         self.file_handler = file_handler
         self.chunk_size = chunk_size
 
-    def count_characters_in_chunk(self, start, size):
+    def count_all_in_chunk(self, start, size):
         chunk = self.file_handler.read_chunk(start, size)
-        return len(chunk)     
+        
+        letters = sum(1 for char in chunk if char.isalpha())
+        digits = sum(1 for char in chunk if char.isdigit())
+        spaces = sum(1 for char in chunk if char == ' ')
+        
+        return {"letters": letters, "digits": digits, "spaces": spaces}    
